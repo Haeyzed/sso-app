1c4a582eb8c2d6e6d60669febeca31f7f02d4bcb
Muibi Azeez Abolade <muibi.azeezabolade@gmail.com>
2024-04-24 18:40:31 +0100

    modified auth

--- app/[lang]/(main)/dashboard/users/page.tsx
+++ app/[lang]/(main)/dashboard/users/page.tsx
@@ -1,10 +1,13 @@
 import BreadCrumb from '@/components/breadcrumbs'
+import { UserClient } from '@/components/main/table/users-table/user-client'
+import { users } from '@/constants/data'
 import React from 'react'
 
 export default function Users() {
   return (
     <div className='flex-1 space-y-4  p-4 pt-6 md:p-8'>
       <BreadCrumb />
+      <UserClient data={users}/>
     </div>
   )
 }
