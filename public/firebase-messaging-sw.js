47042735a0b496725321172a0848098276b0629a
Muibi Azeez Abolade <muibi.azeezabolade@gmail.com>
2024-04-28 14:46:04 +0100

    modified auth

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,15 +8,16 @@ const defaultConfig = {
   appId: true
 }
 
-firebase.initializeApp(firebaseConfig)
-
+firebase.initializeApp(defaultConfig)
 const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage(payload => {
+  console.log('Received background message: ', payload)
+
   const notificationTitle = payload.notification.title
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.image
+    icon: payload.notification.icon || '/icon.png'
   }
 
   self.registration.showNotification(notificationTitle, notificationOptions)
